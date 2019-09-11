7265aa3a31038ca757d57fc5dee93fa005922422
Ryan Coleman <ryan@puppet.com>
2019-09-11 11:27:46 -0700

    copy tf infra to sock shop

---
+++ sock-shop/infra/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">= 0.11.11"
+  backend "gcs" {
+    bucket = "ryan-socks"
+    prefix = "demo"
+  }
+}
+
+provider "google" {
+  credentials = "${var.google-credentials}"
+  project     = "ryan-alliances"
+  region      = "us-west1"
+  zone        = "us-west1-b"
+}
+
+resource "google_container_cluster" "k8sexample" {
+  name               = "k8sexample-cluster"
+  description        = "example k8s cluster"
+  location           = "${local.workspace["gcp_location"]}"
+  initial_node_count = "${var.initial_node_count}"
+  enable_kubernetes_alpha = "true"
+  enable_legacy_abac = "true" 
+
+  master_auth {
+    username = "${var.master_username}"
+    password = "${var.master_password}"
+
+    client_certificate_config {
+      issue_client_certificate = true
+    }
+  }
+
+  node_config {
+    machine_type = "${var.node_machine_type}"
+    disk_size_gb = "${var.node_disk_size}"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+  }
+}
